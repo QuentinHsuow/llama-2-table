7b67eada2696e2aac435997f3b64e9436b517b91
QuentinHsuow <QuentinHsu>
2023-09-20 13:32:19 +0800

    fix: fix rand_int bug

--- process_dataset/data_augmentation.py
+++ process_dataset/data_augmentation.py
@@ -3,7 +3,7 @@ import random
 
 def delete_snd(data):
     row_number = data['answer']
-    to_delete = random.randint(0, row_number)
+    to_delete = random.randint(1, row_number)
     new_rows = []
     for index, row in enumerate(data['rows']):
         if index < row_number - to_delete or index >= row_number:
