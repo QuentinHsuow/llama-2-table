2d43b22f10603a47882bec56c3bfd4c1bcaa20cf
QuentinHsuow <QuentinHsu>
2023-09-21 16:47:37 +0800

    test: test repli

--- process_dataset/subtask1.py
+++ process_dataset/subtask1.py
@@ -160,7 +160,7 @@ def run(feature_file: str):
 
     # get output
     output = []
-    for table in tqdm(tables[:4]):
+    for table in tqdm(tables[:1]):
         data = get_output_from_table_one(table, dic)
         if data:
             data_json = transform_json(data)
