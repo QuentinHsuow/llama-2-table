d8a36a0e801a95cef08bb3125da7b6c846cc4347
QuentinHsuow <QuentinHsu>
2023-09-21 14:48:34 +0800

    test: test 10 sample training

--- process_dataset/subtask1.py
+++ process_dataset/subtask1.py
@@ -160,7 +160,7 @@ def run(feature_file: str):
 
     # get output
     output = []
-    for table in tqdm(tables):
+    for table in tqdm(tables[:10]):
         data = get_output_from_table_one(table, dic)
         if data:
             data_json = transform_json(data)
