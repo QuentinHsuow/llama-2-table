939f8cd987558b0b7fb37fc65538ad223d3a1cfd
QuentinHsuow <QuentinHsu>
2023-09-25 14:10:40 +0800

    fix: fix inference prompt bug

--- inference/test_full_tasks.py
+++ inference/test_full_tasks.py
@@ -6,7 +6,7 @@ def main():
     with open("saved_result/result.json", 'r') as f:
         result = json.load(f)
     for data in result:
-        data['res_type'] = get_type(data['result1'], data['result2'], data['result3'])
+        data['res_type'] = get_type(int(data['result1']), data['result2'], data['result3'])
         if data['res_type'] == data['type']:
             correct += 1
     print(f"Correct: {correct}, percentage: {correct/len(result)}")
