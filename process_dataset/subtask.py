f8327666c8a4f5ad9c513bd6d7752bbdb98cf6a4
QuentinHsuow <quentinhsuow@gmail.com>
2023-09-27 08:52:11 +0800

    fix: fix inference error

--- process_dataset/subtask.py
+++ process_dataset/subtask.py
@@ -233,11 +233,12 @@ def run(feature_file, tokenizer):
 def main(
         model_name: str
 ):
-    tokenizer = LlamaTokenizer.from_pretrained(model_name)
+    tokenizer = LlamaTokenizer.from_pretrained(os.path.join('/spot/v-qinyuxu'), model_name)
     tokenizer.add_special_tokens({"pad_token": "<PAD>"})
     tokenizer.add_tokens(special_tokens, special_tokens=True)
     for file in ['train_row_feature', 'test_263_row_feature']:
         run(file, tokenizer)
+
 
 if __name__ == '__main__':
     fire.Fire(main)
