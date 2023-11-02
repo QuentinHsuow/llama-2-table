4170ec64d1f90f81d8fda3c31d96763e2cb234d1
QuentinHsuow <quentinhsuow@gmail.com>
2023-11-02 08:47:41 +0800

    modify report

--- src/llama_recipes/datasets/tabledetection_dataset.py
+++ src/llama_recipes/datasets/tabledetection_dataset.py
@@ -24,7 +24,7 @@ class TableDetectionDataset(Dataset):
         IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
         table_item = self.tables[index]
         prompt = table_item['prompt']
-        example = prompt + table_item[f'answer']
+        example = prompt + table_item['answer']
         prompt = torch.tensor(
             self.tokenizer.encode(prompt), dtype=torch.int64
         )
@@ -33,25 +33,15 @@ class TableDetectionDataset(Dataset):
         example = torch.tensor(
             example, dtype=torch.int64
         )
-        padding = self.max_words - example.shape[0]
-        if padding > 0:
-            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
-        elif padding < 0:
-            example = example[: self.max_words]
         labels = copy.deepcopy(example)
         labels[: len(prompt)] = -1
         example_mask = example.ge(0)
         label_mask = labels.ge(0)
         example[~example_mask] = 0
         labels[~label_mask] = IGNORE_INDEX
-        example_mask = example_mask.float()
-        label_mask = label_mask.float()
-        print(labels.shape)
-        print(example.shape)
-        print(example_mask.shape)
 
         return {
-            "input_ids": example,
-            "labels": labels,
-            "attention_mask": example_mask,
+            "input_ids": example.to_list(),
+            "labels": labels.to_list(),
+            "attention_mask": example_mask.to_list(),
         }
